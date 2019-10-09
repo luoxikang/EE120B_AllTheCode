9381cbbaffa8a2e5f298e28ce2020157a903b0c4
vingo <wangwenguang98@gmail.com>
2019-10-09 15:43:48 +0800

    Ex2 Passed

---
+++ Lab3/xluo039_lab3_part2.c
@@ -0,0 +1,69 @@
+/*	Author: ee120
+ *  Partner(s) Name: Vingo
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+   
+int main(void){
+
+    DDRA = 0x00; DDRC = 0xFF;
+    PORTA = 0xFF; PORTC = 0x00;
+    while(1){
+        switch (PINA & 0x0F) {
+        case 1:  
+        PORTC =  0x60;
+        break;
+        case 2:
+        PORTC =  0x60;
+        break;
+        case 3:
+        PORTC =  0x70;
+        break;
+        case 4:
+        PORTC =  0x70;
+        break;
+        case 5:
+        PORTC =  0x38;
+        break;
+        case 6:
+        PORTC =  0x38;
+        break;
+        case 7:
+        PORTC =  0x3C;
+        break;
+        case 8:
+        PORTC =  0x3C;
+        break;
+        case 9:
+        PORTC =  0x3C;
+        break;
+        case 10:
+        PORTC =  0x3E;
+        break;
+        case 11:
+        PORTC =  0x3E;
+        break;
+        case 12:
+        PORTC =  0x3E;
+        break;
+        case 13:
+        PORTC =  0x3F;
+        break;
+        case 14:
+        PORTC =  0x3F;
+        break;
+        case 15:
+        PORTC =  0x3F;
+        break;
+        }
+    }
+    return 0;
+} 
