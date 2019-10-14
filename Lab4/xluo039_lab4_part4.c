8143192c790417723b7be0808163e4e0c0f13d6f
XikangLuo <luoxikang@csu.edu.cn>
2019-10-14 04:17:33 -0700

    Not tested  all

--- Lab4/xluo039_lab4_part4.c
+++ Lab4/xluo039_lab4_part4.c
@@ -9,17 +9,17 @@
 typedef  enum KeyStates{Start ,Press, Wait} KeyState;
 KeyState Stick(KeyState State);
 
-char[MAXNUM] KeyQueue = {0};
+char KeyQueue[MAXNUM] = {'N'};
 void Record(char key);
 void Check();
+KeyState State = Start;
 
 int main(void){
     DDRA = 0x00; DDRB = 0xFF;
     PORTA = 0xFF; PORTB = 0x00;
 
     while(1){
-        KeyState State = Start;
-        State = Stick(State);
+	State = Stick(State);
     }
 
     return 1;
@@ -29,12 +29,12 @@ KeyState Stick(KeyState State){
     unsigned char X = PINA & 0x01;
     unsigned char Y = PINA & 0x02;
     unsigned char Shape = PINA & 0x04;
-    unsigned char Button = PINA & 0x80; 
+    unsigned char Button = PINA & 0x08; 
 
     switch(State){
 
         case Start:{
-            State  = Wait_C;
+            State  = Wait;
             break;
         }
             
@@ -51,50 +51,65 @@ KeyState Stick(KeyState State){
             }
             else if (Shape)
             {
-                State = Press；
+                State = Press; 
                 Record('#');
             }
             else if (Button)
             {
                 State = Press;
-                Record('B') // B is Button
+                Record('B'); // B is Button
             }
             else
             {
                 State = State;
             }
+            
 
         } 
 
         case Press:{
-            if !(X || Y || Button || Shape){
+            if (!(X || Y || Button || Shape)){
                 State = Wait;
+                Check();
             }
 
         }
 
-        Check();
+        
     }
 
-    void Record(char Key){
-        for (int i = 0; i < MAXNUM-1; i++){
+    return State;
+}
+
+void Record(char Key){
+	unsigned char i = 0;
+        while (i < MAXNUM-1){
             KeyQueue[i+1] = KeyQueue[i];
+	    i++;
         }
         KeyQueue[0] = Key;
-    }
+}
 
-    void Check(){
-            if (KeyQueue[0] == 'Y' && KeyQueue[1] == '#' ){
-                PORTB = 0x01; // Unlock
-                KeyQueue = {0};
-            } else if (KeyQueue[0] = 'B')       
-            {
-                PORTB = 0x00; // Lock
-                KeyQueue = {0};
-            } else
-            {
-                ////// 
-            }
-                     
-    }
+void Check(void){
+        if (KeyQueue[0] == 'Y' && KeyQueue[1] == '#'  ){
+            // if (PORTB != 0x01){
+            //     PORTB = 0x00;
+            // } else
+            // {
+            //     PORTB = 0x01;
+            // }
+
+            PORTB = (~PORTB) & 0x01;
+
+            // PORTB = 0x01;
+            
+            // PORTB = !PORTB; // Unlock - Knock Change
+        } else if (KeyQueue[0] == 'B')       
+        {
+            PORTB = 0x00; // Lock
+        } else
+        {
+            ////// 
+        }
+                    
 }
